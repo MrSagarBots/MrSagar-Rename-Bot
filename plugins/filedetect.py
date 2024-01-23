d4f79bc5a79c1dcca7b8bb57b72ae22972e7c9ac
Mr Sagar <143385461+MrSagarBots@users.noreply.github.com>
2024-01-23 20:46:04 +0530

    Bugs Solved

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -50,6 +50,6 @@ async def refunc(client,message):
         		else:
         			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📁 Document",callback_data = "doc") ]])
         		# dont chenge this message.reply_text 
-        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- ```{out_filename}```",
+        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- `{out_filename}`",
         		reply_to_message_id=mg_id,reply_markup = markup)
         		
