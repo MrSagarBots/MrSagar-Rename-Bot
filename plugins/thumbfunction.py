a582763d05af05a2ab224247b6ce81a25afc8fa0
Mr Sagar <143385461+MrSagarBots@users.noreply.github.com>
2024-05-21 14:55:38 +0530

    Update thumbfunction.py

--- plugins/thumbfunction.py
+++ plugins/thumbfunction.py
@@ -14,7 +14,7 @@ async def viewthumb(client,message):
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client,message):
 	delthumb(int(message.chat.id))
-	await message.reply_text("**ᴛʜᴜᴍʙɴᴀɪʟ  ᴅᴇʟᴇᴛᴇᴅ  ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ.**")
+	await message.reply_text("**ᴛʜᴜᴍʙɴᴀɪʟ  ᴅᴇʟᴇᴛᴇᴅ  ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ❌**")
 
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client,message):
